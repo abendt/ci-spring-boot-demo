41f37ada60da9c1c5059c7d54d4374f7663e244b
Alphonse Bendt <alphonse.bendt@idealo.de>
2017-05-08 17:26:19 +0200

    stop container before test

---
+++ src/main/scripts/docker-stop.groovy
@@ -0,0 +1,19 @@
+def dockerHost = properties['dockerHost']
+
+if (dockerHost == '127.0.0.1') {
+    println("local docker")
+
+    println "docker rm -f ci-demo".execute().text
+} else {
+    println("remote docker")
+
+    def baseUrl = new URL("http://" + dockerHost + ":4243/containers/ci-demo?force=true")
+
+    def connection = baseUrl.openConnection()
+    connection.with {
+        doOutput = true
+        requestMethod = 'DELETE'
+        println content.text
+    }
+}
+
