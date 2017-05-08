7e1e13c6db957f85dd71647d855a6bd74f4026c0
Alphonse Bendt <alphonse.bendt@idealo.de>
2017-05-08 17:54:54 +0200

    stop container before test

--- src/main/scripts/docker-stop.groovy
+++ src/main/scripts/docker-stop.groovy
@@ -1,5 +1,8 @@
 import groovy.json.JsonSlurper
 
+def dockerHost = project.properties['dockerHost']
+def dockerUrl = "http://$dockerHost:4243"
+
 def listContainers() {
     def baseUrl = new URL("$dockerUrl/containers/json")
 
@@ -15,32 +18,33 @@ def listContainers() {
     return result
 }
 
-def dockerHost = project.properties['dockerHost']
-def dockerUrl = "http://$dockerHost:4243"
+def stopContainer() {
+    if (dockerHost == '127.0.0.1') {
+        println("local docker")
 
-if (dockerHost == '127.0.0.1') {
-    println("local docker")
+        println "docker rm -f ci-demo".execute().text
+    } else {
+        println("remote docker")
 
-    println "docker rm -f ci-demo".execute().text
-} else {
-    println("remote docker")
+        println listContainers()
 
-    println listContainers()
+        def baseUrl = new URL("$dockerUrl/containers/ci-demo?force=true")
 
-    def baseUrl = new URL("$dockerUrl/containers/ci-demo?force=true")
+        def connection = baseUrl.openConnection()
 
-    def connection = baseUrl.openConnection()
+        try {
+            connection.with {
+                doOutput = true
+                requestMethod = 'DELETE'
+                println content.text
+            }
+        } catch (Exception e) {
+            e.printStackTrace()
 
-    try {
-        connection.with {
-            doOutput = true
-            requestMethod = 'DELETE'
-            println content.text
+            throw e
         }
-    } catch (Exception e) {
-        e.printStackTrace()
-
-        throw e
     }
 }
+
+stopContainer()
 
