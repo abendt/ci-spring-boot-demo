1fbe69e1fcf29621039dbd20f774dc9bca84ae30
Alphonse Bendt <alphonse.bendt@idealo.de>
2017-05-24 11:07:03 +0200

    use mongodb persistence

--- lambdacd-pipeline/project.clj
+++ lambdacd-pipeline/project.clj
@@ -9,6 +9,8 @@
                            [org.clojure/tools.logging "0.3.1"]
                            [org.slf4j/slf4j-api "1.7.5"]
                            [ch.qos.logback/logback-core "1.0.13"]
-                           [ch.qos.logback/logback-classic "1.0.13"]]
+                           [ch.qos.logback/logback-classic "1.0.13"]
+                           [lambdacd-mongodb "2.0.0"]
+                           ]
             :profiles {:uberjar {:aot :all}}
             :main lambdacd-pipeline.core)
--- lambdacd-pipeline/src/lambdacd_pipeline/core.clj
+++ lambdacd-pipeline/src/lambdacd_pipeline/core.clj
@@ -6,23 +6,46 @@
     [lambdacd.runners :as runners]
     [lambdacd.util :as util]
     [lambdacd.core :as lambdacd]
+    [lambdacd-mongodb.mongodb-state :as mongodb-state]
     [clojure.tools.logging :as log])
   (:gen-class))
 
 (defn -main [& args]
-  (let [;; the home dir is where LambdaCD saves all data.
+  (let [
+        pipeline pipeline/pipeline-def
+
+        ;; the home dir is where LambdaCD saves all data.
         ;; point this to a particular directory to keep builds around after restarting
         home-dir (util/create-temp-dir)
-        config   {:home-dir home-dir
-                  :name     "lambdacd pipeline"}
+
+        mongodb-cfg {
+                     :uri                                 "mongodb://localhost:27017/lambdacd"
+                     :hosts                               ["localhost"]
+                     :port                                27017
+                     :db                                  "lambdacd"
+                     :col                                 "test-project"
+                     :max-builds                          10
+                     :ttl                                 7
+                     :mark-running-steps-as               :killed
+                     :pipeline-def                        pipeline
+                     :persist-the-output-of-running-steps false
+                     :use-readable-build-numbers          true}
+
+        config {:mongodb-cfg mongodb-cfg
+                :home-dir    home-dir
+                :name        "lambdacd pipeline"}
+
         ;; initialize and wire everything together
-        pipeline (lambdacd/assemble-pipeline pipeline/pipeline-def config)
+        pipeline (lambdacd/assemble-pipeline pipeline config (mongodb-state/new-mongodb-state config))
+
         ;; create a Ring handler for the UI
-        app      (ui-selection/ui-routes pipeline)]
+        app (ui-selection/ui-routes pipeline)]
     (log/info "LambdaCD Home Directory is " home-dir)
+
     ;; this starts the pipeline and runs one build after the other.
     ;; there are other runners and you can define your own as well.
     (runners/start-one-run-after-another pipeline)
+
     ;; start the webserver to serve the UI
     (http-kit/run-server app {:open-browser? false
                               :port          8090})))
